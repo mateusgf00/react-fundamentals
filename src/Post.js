2e90ad5daa9a0074a55323885b2b3cfd87b68c81
Mateus <mateusgf00@gmail.com>
2022-09-14 15:53:25 -0300

    created dinamic class

--- src/Post.js
+++ src/Post.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import PostHeader from './PostHeader';
 
+import styles from './Post.scss';
+
 function Post(props) {
   return (
-    <>
-      <article>
-        <PostHeader 
+    <article className={
+      props.post.removed ? styles.postDeleted : styles.post
+
+    }>
+      <PostHeader
         onRemove={props.onRemove}
         post={{
           id: props.post.id,
           title: props.post.title,
           read: props.post.read,
         }}
-        />
-        <br />
-        <small>{props.post.subtitle}</small><br />
-        <small>{props.post.likes}</small>
-      </article>
+      />
       <br />
-    </>
+      <small>{props.post.subtitle}</small>
+      <br />
+      <small>{props.post.likes}</small>
+    </article>
+
   )
 }
 
